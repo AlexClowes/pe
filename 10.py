57e08bff894c6b5959396bb663a4a7e8abaeff75
AlexClowes <alexclowes@gmail.com>
2019-10-03 10:54:34 +0100

    Problem 10

---
+++ 10.py
@@ -0,0 +1,9 @@
+from utils import get_primes_up_to_n
+
+
+def main():
+    print(sum(get_primes_up_to_n(2 * 10**6)))
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def is_palindrome(n):
     str_rep = str(n)
     return str_rep == str_rep[::-1]
@@ -28,3 +31,17 @@ def get_first_n_primes(n):
             primes.append(test_num)
         test_num += 2
     return primes
+
+
+def get_primes_up_to_n(n):
+    # Sieve of eratosthenes
+    candidates = range(2, n+1)
+    is_prime = [True] * len(candidates)
+    for pos, val in enumerate(candidates):
+        # If val is prime, then all its multiples are not prime
+        if is_prime[pos]:
+            mult_pos = pos + val
+            while mult_pos < len(candidates):
+                is_prime[mult_pos] = False
+                mult_pos += val
+    return itertools.compress(candidates, is_prime)
