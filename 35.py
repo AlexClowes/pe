71a8d201120b7d1dd29eb6ade3df20e1552856b9
AlexClowes <alexclowes@gmail.com>
2019-10-04 13:46:26 +0100

    Speed up problem 35

--- 35.py
+++ 35.py
@@ -9,12 +9,13 @@ def cycle_number_gen(n):
 
 def main():
     primes = set(get_primes_up_to_n(10**6))
-    count = 0
+    count = 2 # Count 2 and 5, excluded by checks below
     for n in range(1, 10**6):
+        if any((d % 2 == 0 or d == 5) for d in map(int, str(n))):
+            continue
         if all(c in primes for c in cycle_number_gen(n)):
             count += 1
     print(count)
-
 
 
 if __name__ == "__main__":
