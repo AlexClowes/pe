27042d43e1877074f97e9ce5b65f3b3421363811
AlexClowes <alexclowes@gmail.com>
2019-10-07 13:29:00 +0100

    Problem 33

--- 33.py
+++ 33.py
@@ -2,29 +2,28 @@ from fractions import Fraction
 
 
 def cancel(num, den):
-    num = list(str(num))
-    den = list(str(den))
-    done = False
+    num_digits = divmod(num, 10)
+    den_digits = divmod(den, 10)
     for i in range(2):
         for j in range(2):
-            if not done and num[i] == den[j] and num[i] != 0:
-                del num[i]
-                del den[j]
-                done = True
-    num = int(''.join(num))
-    den = int(''.join(den))
+            if num_digits[i] == den_digits[j]:
+                return num_digits[(i + 1) % 2], den_digits[(j + 1) % 2]
     return num, den
 
 
 def main():
-    prod = Fraction(1, 1)
-    for num in range(10, 99):
-        for den in range(num + 1, 100):
-            c_num, c_den = cancel(num, den)
-            if num != c_num and num / den == c_num / c_den:
-                prod *= Fraction(num, den)
-                print(num, den, c_num, c_den)
-    print(prod)
+    product = Fraction(1)
+    for p in range(10, 100):
+        if p % 10 == 0:
+            continue
+        for q in range(p + 1, 100):
+            if q % 10 == 0:
+                continue
+            p_c, q_c = cancel(p, q)
+            if p != p_c and p * q_c == p_c * q:
+                product *= Fraction(p, q)
+    print(product)
+
 
 
 if __name__ == "__main__":
