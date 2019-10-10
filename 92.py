a603f948f9dca28f9d2ccb774d8719a877e7f541
Alex Clowes <alexclowes@gmail.com>
2019-10-10 21:53:48 +0100

    Problem 92

---
+++ 92.py
@@ -0,0 +1,23 @@
+import functools
+
+
+@functools.lru_cache(maxsize=None)
+def ends_in_89(n):
+    if n == 1:
+        return False
+    if n == 89:
+        return True
+    ret = 0
+    while n > 0:
+        n, r = divmod(n, 10)
+        ret += r * r
+    return ends_in_89(ret)
+
+
+def main():
+    print(sum(ends_in_89(n) for n in range(1, 10**7)))
+
+
+if __name__ == "__main__":
+    main()
+
