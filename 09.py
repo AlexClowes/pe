80d2854a7b401e26130c3266509a7ec1e8a3c8d6
AlexClowes <alexclowes@gmail.com>
2019-10-21 16:46:55 +0100

    Remove slow method, unnecessary prints

--- 09.py
+++ 09.py
@@ -2,24 +2,12 @@ import time
 
 
 def main():
-    start = time.time()
-    for a in range(1, 1000):
-        for b in range(a, 1000):
-            c = 1000 - a - b
-            if a**2 + b**2 == c**2:
-                print(a, b, c)
-                print(a * b * c)
-    print(time.time() - start)
-
-    start = time.time()
     for b in range(1, 1000):
         q, r = divmod(500000 - 1000 * b, 1000 - b)
         if r == 0 and 0 < q < b:
             a = q
             c = 1000 - a - b
-            print(a, b, c)
             print(a * b * c)
-    print(time.time() - start)
 
 
 if __name__ == "__main__":
