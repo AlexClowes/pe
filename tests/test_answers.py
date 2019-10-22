364d8b95a2f71698537c2453854687d169c99ea7
AlexClowes <alexclowes@gmail.com>
2019-10-22 13:18:21 +0100

    Improve test naming, use ids

--- tests/test_answers.py
+++ tests/test_answers.py
@@ -7,10 +7,12 @@ import pytest
 
 csv_path = os.path.join(os.path.dirname(__file__), "answers.csv")
 df = pd.read_csv(csv_path)
+ids = [f"{n:02}" for n in df.values[:, 0]]
 
 
-@pytest.mark.parametrize("problem_no,answer", zip(df["problem_no"], df["answer"]))
-def test_n(problem_no, answer, capsys):
+
+@pytest.mark.parametrize("problem_no, answer", df.values, ids=ids)
+def test_problem_solution(problem_no, answer, capsys):
     import_module(f"{problem_no:02}").main()
     out, err = capsys.readouterr()
     assert out.strip() == str(answer)
