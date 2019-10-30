7daf08073362a210ab5008dd9eb14239746592c3
AlexClowes <alexclowes@gmail.com>
2019-10-30 13:56:40 +0000

    Small speed up for problem 357

--- 357.py
+++ 357.py
@@ -4,18 +4,20 @@ from utils import get_primes_up_to_n
 
 
 def get_factors(n):
-    for i in range(2, floor(sqrt(n)) + 1):
+    for i in range(3, floor(sqrt(n)) + 1):
         if n % i == 0:
             yield i
 
 
 def main():
     primes = set(get_primes_up_to_n(10 ** 8))
-    total = 0
+    total = 1
     for p in primes:
+        if p % 4 != 3:
+            continue
         candidate = p - 1
         factors = get_factors(candidate)
-        if all(f + candidate / f in primes for f in factors):
+        if candidate // 2 + 2 in primes and all(f + candidate / f in primes for f in factors):
             total += candidate
     print(total)
 
