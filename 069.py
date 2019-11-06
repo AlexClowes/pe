a9361014ba995e70137db36f28243c5b76761554
AlexClowes <alexclowes@gmail.com>
2019-11-06 17:52:28 +0000

    Speed up problem 69

--- 069.py
+++ 069.py
@@ -1,23 +1,14 @@
-from utils import get_prime_factors
-
-
-def euler_totient(n):
-    prime_factors = set(get_prime_factors(n))
-    prod = n
-    for p in prime_factors:
-        prod *= (p - 1)
-        prod //= p
-    return prod
+from utils import prime_generator
 
 
 def main():
-    best_ratio = 0
-    for n in range(2, 10**6 + 1):
-        ratio = n / euler_totient(n)
-        if ratio > best_ratio:
-            best_ratio = ratio
-            best_n = n
-    print(best_n)
+    n = 1
+    for p in prime_generator():
+        next_n = n * p
+        if next_n > 10 ** 6:
+            break
+        n = next_n
+    print(n)
 
 
 if __name__ == "__main__":
