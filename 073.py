41bd09f459cc7848bd40dfe5cb173599c7f2979f
AlexClowes <alexclowes@gmail.com>
2020-01-07 14:22:14 +0000

    Speed up problem 73

--- 073.py
+++ 073.py
@@ -4,8 +4,8 @@ from math import gcd
 def main():
     total = 0
     for d in range(1, 12001):
-        for n in range(1, d + 1):
-            if 2 * n < d < 3 * n and gcd(n, d) == 1:
+        for n in range(d // 3 + 1, (d + 1) // 2):
+            if gcd(n, d) == 1:
                 total += 1
     print(total)
 
