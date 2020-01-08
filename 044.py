6bcbf626da95c98ae605e8be316506e35b08fc75
AlexClowes <alexclowes@gmail.com>
2020-01-08 16:40:58 +0000

    Speed up problem 44

--- 044.py
+++ 044.py
@@ -1,9 +1,11 @@
 def main():
-    pent_nums = {n * (3 * n - 1) // 2 for n in range(1, 10**4)}
-    for a in pent_nums:
-        for b in pent_nums:
-            if a + b in pent_nums and a - b in pent_nums:
+    pent_list = [n * (3 * n - 1) // 2 for n in range(1, 10 ** 4)]
+    pent_set = set(pent_list)
+    for i, a in enumerate(pent_list):
+        for b in pent_list[:i]:
+            if a + b in pent_set and a - b in pent_set:
                 print(a - b)
+                return
 
 
 if __name__ == "__main__":
