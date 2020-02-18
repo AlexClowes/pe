ddf975816e1704e3f774915a989f61c9fe8ba6b5
AlexClowes <alexclowes@gmail.com>
2020-02-18 13:57:44 +0000

    Speed up problem 381 with numba

--- 381.py
+++ 381.py
@@ -1,14 +1,21 @@
-from utils import get_primes_up_to_n
+from numba import vectorize
+import numpy as np
+
+from utils import get_primes_up_to_n, powmod
 
 
+@vectorize("int32(int32)", target="parallel")
 def S(p):
-    inv_2 = pow(2, p - 2, p)
-    inv_3 = pow(3, p - 2, p)
-    return (-inv_2 + inv_2 * inv_3 - inv_2 ** 3 * inv_3) % p
+    inv_2 = powmod(2, p - 2, p)
+    inv_6 = powmod(6, p - 2, p)
+    inv_24 = (inv_2 * inv_2 * inv_6) % p
+    inv_24 = (((inv_2 * inv_2) % p) * inv_6) % p
+    return (-inv_2 + inv_6 - inv_24) % p
 
 
 def main():
-    print(sum(S(p) for p in get_primes_up_to_n(10 ** 8) if p >= 5))
+    primes = np.fromiter(get_primes_up_to_n(10 ** 8), dtype=np.int32)[2:]
+    print(np.sum(S(primes)))
 
 
 if __name__ == "__main__":
