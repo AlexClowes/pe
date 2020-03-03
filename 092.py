3aa4363cb705af8b8d3e3c8e8a04bee89e97e62e
AlexClowes <alexclowes@gmail.com>
2020-03-03 11:21:29 +0000

    Remove recursion and use numba to compile sum

--- 092.py
+++ 092.py
@@ -3,19 +3,21 @@ from numba import njit
 
 @njit
 def ends_in_89(n):
-    if n == 1:
-        return False
-    if n == 89:
-        return True
-    digit_square_sum = 0
-    while n > 0:
-        n, r = divmod(n, 10)
-        digit_square_sum += r * r
-    return ends_in_89(digit_square_sum)
+    while n != 1 and n != 89:
+        digit_square_sum = 0
+        while n > 0:
+            n, r = divmod(n, 10)
+            digit_square_sum += r * r
+        n = digit_square_sum
+    return n == 89
 
 
+@njit
 def main():
-    print(sum(ends_in_89(n) for n in range(1, 10**7)))
+    count = 0
+    for n in range(1, 10 ** 7):
+        count += ends_in_89(n)
+    print(count)
 
 
 if __name__ == "__main__":
