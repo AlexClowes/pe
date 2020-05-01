d2ecb6e9c9b380829c11035bae5c4d4b9c52612b
AlexClowes <alexclowes@gmail.com>
2020-05-01 09:19:09 +0100

    Speed up problem 216

--- 216.py
+++ 216.py
@@ -36,7 +36,12 @@ def count_primes(n_max):
     tn_is_prime = np.ones(n_max + 1, dtype=np.bool)
     primes = [p for p in get_primes_up_to_n(int(2 ** 0.5 * n_max)) if p % 8 in (1, 7)]
     for p in primes:
-        r = tonelli_shanks(p, (p + 1) // 2)
+        if p % 8 == 7:
+            # In this case, square root is easy to find
+            r = powmod((p - 1) // 2, (p + 1) // 4, p)
+            r = min(r, p - r)
+        else:
+            r = tonelli_shanks(p, (p + 1) // 2)
         tn_is_prime[p - r :: p] = False
         tn_is_prime[p + r :: p] = False
     return np.sum(tn_is_prime[2:])
